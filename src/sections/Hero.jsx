9751801791c2154ca10383ec57cc7351138d99d8
shantanu saraf <shantanusaraf4@gmail.com>
2025-02-11 14:18:10 +0530

    update 3d model and hero section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,78 @@
+import { PerspectiveCamera } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+import HackerRoom from "../components/HackerRoom"
+import CanvasLoader from "../components/CanvasLoader"
+import { Suspense } from 'react';
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
+import Target from "../components/Target.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Cube from "../components/Cube.jsx";
+import Rings from "../components/Rings.jsx";
+import HeroCamera from "../components/HeroCamera.jsx";
+
+
+
+const Hero = () => {
+
+
+
+  const isSmall = useMediaQuery({maxWidth: 440})
+  const isMobile = useMediaQuery({maxWidth: 768})
+  const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024})
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet)
+
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      
+        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+            
+            <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hii, I'm Shantanu <span className="waving-hand">👋</span></p>
+            
+            <p className="hero_tag text-gray_gradient">
+                Building Frontend & Backend with ❤️
+            </p>
+
+        </div>
+
+        <div className="w-full h-full absolute inset-0">
+          
+        {/* <Leva /> */}
+
+            <Canvas className="w-full h-full">
+
+                <Suspense fallback={<CanvasLoader />}>
+
+                    <PerspectiveCamera makeDefault position={[0, 2, 20]}/>
+                  
+                    <HeroCamera isMobile={isMobile}>
+                        <HackerRoom 
+                        position={sizes.deskPosition}
+                        rotation={[0, -Math.PI, 0]}
+                        scale={sizes.deskScale}
+                        
+                        />
+                      </HeroCamera>
+
+                    <group>
+                      <Target position={sizes.targetPosition} />
+                      <ReactLogo position={sizes.reactLogoPosition}/>
+                      <Cube position={sizes.cubePosition} />
+                      <Rings position={sizes.ringPosition} />
+                    </group>
+
+                    <ambientLight intensity={1} />
+                    <directionalLight position={[10, 10, 10]} intensity={0.5} />
+
+                </Suspense>
+
+            </Canvas>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default Hero
