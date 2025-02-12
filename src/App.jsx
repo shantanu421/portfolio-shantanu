dad177f218347f6f3bda9ca1dda9a783f1f02a35
shantanu saraf <shantanusaraf4@gmail.com>
2025-02-12 21:24:02 +0530

    update loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import "./index.css"
+
 
 const App = () => {
+
   return (
+    
     <main className='max-w-7xl mx-auto'>
+     
       <Navbar />
       <Hero />
+  
     </main>
   )
 }
