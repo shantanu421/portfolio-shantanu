a3bb19f6e4fba91b3604e67e78dc9270d27cc336
shantanu saraf <shantanusaraf4@gmail.com>
2025-02-16 22:36:30 +0530

    add passions grid funtionality

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -82,21 +82,38 @@ const About = () => {
        
 
 
-        <div className="col-span-2 row-span-1">
-          <div className="grid-container">
-            <div className="rounded-3xl w-full sm:h-[220px] h-fit flex justify-center items-center">
-            </div>
+<div className="col-span-2 row-span-1 grid-container flex flex-col items-center justify-center">
+  {/* Title & Subtitle */}
+  <div className="text-center mb-24">
 
-            <div>
-              <p className="grid-headtext">Passion lor sit.</p>
-              <p className="grid-subtext">
-                Lorem ipsum dolor sit amet consectetur adipisicing. Lorem ipsum
-                dolor sit. lor
-              </p>
-              <Button name={"Contact me"} isBeam containerClass={"w-full mt-6"} />
-            </div>
-          </div>
-        </div>
+    <p className="text-2xl font-semibold text-white">
+      Apart from coding, I love to pursue my other passions, like
+    </p>
+
+  </div>
+
+  {/* Hobbies Section */}
+  <div className="grid grid-cols-3 gap-20">
+    {/* Hobby 1 */}
+    <div className="flex flex-col items-center">
+      <img src="/other-logos/space.png" alt="Space" className="w-36 h-36 opacity-50 hover:opacity-75 transition-opacity duration-300" />
+      <p className="text-white mt-2 text-center">Space Exploration</p>
+    </div>
+
+    {/* Hobby 2 */}
+    <div className="flex flex-col items-center">
+      <img src="/other-logos/football.png" alt="Football" className="w-36 h-36 opacity-50 hover:opacity-70 transition-opacity duration-300" />
+      <p className="text-white mt-2 text-center">Playing Football</p>
+    </div>
+
+    {/* Hobby 3 */}
+    <div className="flex flex-col items-center">
+      <img src="/other-logos/writing.png" alt="Writing" className="w-36 h-36 opacity-45 hover:opacity-65 transition-opacity duration-300" />
+      <p className="text-white mt-2 text-center">Writing</p>
+    </div>
+  </div>
+</div>
+
 
         <div className="col-span-1 row-span-1 grid-container flex flex-col items-center justify-center">
   <div className="text-center grid gap-9 ">
