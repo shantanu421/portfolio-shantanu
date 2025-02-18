4d090e54fafa29eacb7eaa3b447604f0f594045b
shantanu saraf <shantanusaraf4@gmail.com>
2025-02-18 16:37:50 +0530

    add footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,25 +1,25 @@
 const Footer = () => {
     return (
-      <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
+      <footer className="c-space pt-5 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
         <div className="text-white-500 flex gap-2">
-          <p>Terms & Conditions</p>
-          <p>|</p>
-          <p>Privacy Policy</p>
+          <p>
+            Designed and Developed with 🖤
+          </p>
         </div>
   
-        <div className="flex gap-3">
-          <div className="social-icon">
-            <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-          </div>
-          <div className="social-icon">
-            <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-          </div>
-          <div className="social-icon">
-            <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-          </div>
+        <div className="flex gap-6">
+          <a href="https://github.com/shantanu421" target="_blank" rel=" noreferrer">
+            <img src="/assets/github.svg" alt="github" className="w-5 h-5" />
+          </a>
+          <a href="https://x.com/shantanu_saraf" target="_blank" rel=" noreferrer">
+            <img src="/assets/twitter.svg" alt="twitter" className="w-5 h-5" />
+          </a>
+          <a href="https://www.linkedin.com/in/shantanusaraf/" target="_blank" rel=" noreferrer">
+            <img src="/assets/linkedin.svg" alt="linkedin" className="w-5 h-5" />
+          </a>
         </div>
   
-        <p className="text-white-500">© 2024 Adrian Hajdin. All rights reserved.</p>
+        <p className="text-white-500">© 2025 Shantanu Saraf</p>
       </footer>
     );
   };
