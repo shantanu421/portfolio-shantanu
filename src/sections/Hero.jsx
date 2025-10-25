e7e2a46744512bcbb3cefb880d13c8dee5165240
shantanu421 <shantanusaraf4@gmail.com>
2025-10-25 13:27:05 +0530

    update name from introduction in hero component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl mt-[-11px] text-2xl font-medium text-white text-center font-generalsans">
-          Hii, I'm Shantanu Saraf<span className="waving-hand">👋</span>
+          Hii, I'm Shantanu<span className="waving-hand">👋</span>
         </p>
 
         <p className="hero_tag text-gray_gradient">
